6bf8432337bed26d6dbe51b8405689ba353c917e
SEC\user <dorothy_007@naver.com>
2019-06-25 03:52:40 +0900

    PhoneForm 생성

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React, {Component} from 'react';
-import Counter from './component/Counter';
+import PhoneForm from './component/PhoneForm';
 
 class App extends Component{
   render(){
     return(
       <div>
-        <Counter />
+        <PhoneForm />
       </div>
     )
   }
