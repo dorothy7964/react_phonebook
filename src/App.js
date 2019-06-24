8e44415f3041313b6f0150d7e0d9b93e1b9f81b9
SEC\user <dorothy_007@naver.com>
2019-06-25 03:50:41 +0900

    Counter 수정
    필요없는 함수 제거

--- src/App.js
+++ src/App.js
@@ -2,10 +2,6 @@ import React, {Component} from 'react';
 import Counter from './component/Counter';
 
 class App extends Component{
-  handleCreate = (data) => {
-    console.log(data);
-  }
-
   render(){
     return(
       <div>
