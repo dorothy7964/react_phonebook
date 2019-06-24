f36593cebfdc331c2bf5b5eb279f2b32feab0621
SEC\user <dorothy_007@naver.com>
2019-06-25 04:42:14 +0900

    데이터 수정

--- src/App.js
+++ src/App.js
@@ -30,6 +30,17 @@ class App extends Component{
     })
   }
 
+  handleUpdate = (id, data) => {
+    const { information } = this.state;
+    this.setState({
+      information: information.map(
+        info => id === info.id
+          ? { ...info, ...data } // 새 객체를 만들어서 기존의 값과 전달받은 data 을 덮어씀
+          : info // 기존의 값을 그대로 유지
+      )
+    })
+  }
+
   render(){
     const { information } = this.state;
     return(
@@ -41,6 +52,7 @@ class App extends Component{
         <PhoneInfoList
           data={information}
           onRemove={this.handleRemove}
+          onUpdate={this.handleUpdate}
         />
       </div>
     )
